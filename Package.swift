bf593e50b69ab9adc381c71208f4321cd2f05c57
David Trallero <ditiem@gmail.com>
2020-09-14 11:41:43 +0200

    Added Crypto to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,13 @@
 
 import PackageDescription
 
+
+
 let package = Package(
     name: "MIOServerKitAWS_S3",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,13 +20,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/miolabs/MIOServerKit.git", .branch("master")),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MIOServerKitAWS_S3",
-            dependencies: ["MIOServerKit"]),
+            dependencies: ["MIOServerKit", "Crypto"]),
         .testTarget(
             name: "MIOServerKitAWS_S3Tests",
             dependencies: ["MIOServerKitAWS_S3"]),
