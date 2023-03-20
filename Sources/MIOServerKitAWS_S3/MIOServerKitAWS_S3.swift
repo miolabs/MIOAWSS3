48044efd2775fe0e13622f595abfe1081e8cf2f6
David Trallero <ditiem@gmail.com>
2023-03-20 20:51:50 +0800

    old code

--- Sources/MIOServerKitAWS_S3/MIOServerKitAWS_S3.swift
+++ Sources/MIOServerKitAWS_S3/MIOServerKitAWS_S3.swift
@@ -2,14 +2,14 @@
 import Foundation
 import MIOServerKit
 
-extension MIOServerKit
-{
-    public static var s3: MSKAWS_S3.Type {
-        get { return MSKAWS_S3.self }
-    }
-}
-
-public class MSKAWS_S3
-{
-    
-}
+//extension MIOServerKit
+//{
+//    public static var s3: MSKAWS_S3.Type {
+//        get { return MSKAWS_S3.self }
+//    }
+//}
+//
+//public class MSKAWS_S3
+//{
+//
+//}
