32ef6243d93405b5d4ca712e3ac1fd5a5f2c279a
Javier Segura <javier@miolabs.com>
2024-09-03 17:08:28 +0200

    upgrading minimum requirements

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     name: "MIOServerKitAWS_S3",
     platforms: [
         .iOS( .v13),
-        .macOS(.v10_15),
+        .macOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
