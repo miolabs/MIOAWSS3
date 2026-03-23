f4946d3c93c0285badc364be71a5ac07fe84b58b
Javier Segura <javier@miolabs.com>
2026-03-23 11:21:13 +0100

    removing mio core dependency

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package( url: "https://github.com/miolabs/MIOCore.git", branch: "master" ),
+        //.package( url: "https://github.com/miolabs/MIOCore.git", branch: "master" ),
         .package( url: "https://github.com/apple/swift-crypto.git", "3.8.0"..<"5.0.0" ),
         .package( url: "https://github.com/awslabs/aws-sdk-swift", from: "1.6.18" )
     ],
